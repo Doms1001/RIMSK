16b50f02c076760569aa483c1c60a79faac44393
Dominic Hermosa <dominichermosa28@gmail.com>
2025-11-09 01:51:48 +0800

    cron.js added

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { ENV } from "./config/env.js";
+import job from "./config/cron.js";
+
 
 const app = express();
 const PORT = ENV.PORT || 5001;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ sucess: true });
